3ce60f5b6f15f6c739976b3d6855a12ee25d9484
Jonatan Batista <jonathanc4@yahoo.com.br>
2024-07-31 10:53:26 -0300

    Versão 2024

--- WinBoltAttribute/boltattribute.cs
+++ WinBoltAttribute/boltattribute.cs
@@ -10,8 +10,8 @@ namespace Tekla.Technology.Akit.UserScript
         [Tekla.Macros.Runtime.MacroEntryPointAttribute()]
         public static void Run(Tekla.Macros.Runtime.IMacroRuntime runtime) {
             string applicationName = "WinBoltAttribute.exe";
-            string applicationFolder1 = @"C:\TeklaStructures\2023.0\Environments\common\macros\modeling";
-            string applicationFolder2 = @"C:\ProgramData\Trimble\TeklaStructures\2023.0\Environments\common\macros\modeling";
+            string applicationFolder1 = @"C:\TeklaStructures\2024.0\Environments\common\macros\modeling";
+            string applicationFolder2 = @"C:\ProgramData\Trimble\TeklaStructures\2024.0\Environments\common\macros\modeling";
             string applicationFile1 = Path.Combine(applicationFolder1, applicationName);
             string applicationFile2 = Path.Combine(applicationFolder2, applicationName);
 
